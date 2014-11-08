e56e8e94129551a9119935f959e5970fbf5616e3
Aaron Lipman <alipman88@gmail.com>
2014-11-07 19:19:18 -0500

    new housemates

--- pawnshop/pawnshop.js
+++ pawnshop/pawnshop.js
@@ -2,7 +2,7 @@ function calc() {
 
 	$('#results').html("");
 
-	array = [['aaron' , $('#aaron').val().replace('$','')],['abby'  , $('#abby').val().replace('$','')],['coori' , $('#coori').val().replace('$','')],['zach'  , $('#zach').val().replace('$','')]];
+	array = [['aaron' , $('#aaron').val().replace('$','')],['abby'  , $('#abby').val().replace('$','')],['ariel' , $('#ariel').val().replace('$','')],['warren'  , $('#warren').val().replace('$','')]];
 
 	var sum = 0;
 	var length = 0;
