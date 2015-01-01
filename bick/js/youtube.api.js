5820afd3921c069fa5cf54bd3dc104e7f55bf27f
Aaron Lipman <alipman88@gmail.com>
2015-01-01 17:14:16 -0500

    add modal w/ video playback control

---
+++ bick/js/youtube.api.js
@@ -0,0 +1,6 @@
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
